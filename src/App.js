30f21ed75fb6cb9183aa9014ba4cc86bfee87dcd
bahaa-abouzahr <bahaa.abouzahr@gmail.com>
2024-05-24 22:21:23 +0200

    update 03

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const initialFriends = [
 ];
 
 function Button({ children, onClick }) {
-  return <button className="button" onClick={onClick}>{children} test</button>
+  return <button className="button" onClick={onClick}>{children}</button>
 }
 
 export default function App() {
